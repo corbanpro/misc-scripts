7738858193dfbdec71d03db752f97ce83a9935f2
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-05-27 14:09:37 -0600

    add a script that will run tests and format them the way I like

---
+++ run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make test | grep -v "no test files" | grep -v "failed to load godotenv" | grep -v '\\' | grep -v "coverage: 0.0%"
+exit_code=${PIPESTATUS[0]}
+
+if [ $exit_code -eq 0 ]; then
+	echo -e "\n${C_BGREEN}All Tests Passed!!!${C_RESET}"
+else
+	echo -e "\n${C_BRED}TESTS FAILED!!!${C_RESET}"
+fi
+
+exit $exit_code

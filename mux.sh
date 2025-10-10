a5daf186713fde1bd1054a9bf2a9415f2c5da2af
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-10-10 09:23:32 -0600

    ignore errors on startup tmuxinator

--- mux.sh
+++ mux.sh
@@ -3,8 +3,8 @@
 export NGINX_CONF=znginx.conf
 export rs_cmd="./release-status.mac"
 
-tmux kill-server
-nginx -s stop
+tmux kill-server 2>/dev/null
+nginx -s stop 2>/dev/null
 
 cd ~/dev/data-fetch/
 make run_deps

85be8ef1ddab03b61730116a57fdee8bed607976
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-10-29 10:50:20 -0600

    fix prodwatch

--- prodwatch.sh
+++ prodwatch.sh
@@ -3,5 +3,5 @@
 if [ -z "$1" ]; then
 	watch "kubectl get pods | grep -v beta"
 else
-	watch "kubectl get pods | grep -v beta -e \"$1\" "
+	watch "kubectl get pods | grep -v beta | grep -e \"$1\" "
 fi

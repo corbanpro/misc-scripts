778c8f09a3ab5536659219c390e06ba1c6e195d6
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-12 16:58:24 -0700

    remove uneeded neovim installation helpers

--- nvim-installation-helpers/linux/install-nvim-latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export DOWNLOAD_URL=https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-$DIR/install-nvim.sh
-
-git -C ~/.config/nvim/ checkout main

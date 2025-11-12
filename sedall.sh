de7f5123bccf027b8e7935dfb3e07aa8655005dd
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-12 09:19:40 -0700

    improve sedall

--- sedall.sh
+++ sedall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-find . -type f -exec sed -i "$1" {} +
+set -euo pipefail
+
+LC_CTYPE=C find . -type f \
+	-not -path '*/.git/*' \
+	-not -path '*/node_modules/*' \
+	-not -path '*/vendor/*' \
+	-exec sed -i "" "$1" {} +

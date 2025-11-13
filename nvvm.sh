f9486559e8b2158c27b4ebc527195b2377b20663
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-12 17:11:54 -0700

    improve installation

--- nvvm.sh
+++ nvvm.sh
@@ -3,9 +3,10 @@
 set -e
 
 VERSIONS_DIR="$HOME/bin/neovim"
+PLATFORM="nvim-macos-arm64"
 
 use() {
-	USER_VERSION=$1
+	REQUESTED_VERSION=$1
 	# Collect directories into an array
 	VERSIONS=()
 	for dir in "$VERSIONS_DIR"/*/; do
@@ -14,10 +15,10 @@ use() {
 	done
 
 	# Check if input is in the list
-	if printf '%s\n' "${VERSIONS[@]}" | grep -qx "$USER_VERSION"; then
+	if printf '%s\n' "${VERSIONS[@]}" | grep -qx "$REQUESTED_VERSION"; then
 		mkdir -p ~/.config/nvvm && touch ~/.config/nvvm/version
-		echo $USER_VERSION >~/.config/nvvm/version
-		echo -e "\n${C_CYAN}Now using version: ${USER_VERSION}${C_RESET}"
+		echo $REQUESTED_VERSION >~/.config/nvvm/version
+		echo -e "\n${C_CYAN}Now using version: ${REQUESTED_VERSION}${C_RESET}"
 	else
 		echo "Invalid version! Available versions:"
 		for v in "${VERSIONS[@]}"; do
@@ -29,27 +30,27 @@ use() {
 
 install() {
 	TEMP_DIR="$VERSIONS_DIR/temp"
-	USER_VERSION=$1
-	DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/$USER_VERSION/nvim-macos-arm64.tar.gz"
-	TAR_PATH="$TEMP_DIR/nvim-macos-arm64.tar.gz"
-	TEMP_OUTPUT_DIR="$TEMP_DIR/nvim-macos-arm64/"
-	OUTPUT_DIR="$VERSIONS_DIR/$USER_VERSION"
+	REQUESTED_VERSION=$1
+	DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/${REQUESTED_VERSION}/${PLATFORM}.tar.gz"
+	TEMP_OUTPUT_DIR="${TEMP_DIR}/${PLATFORM}"
+	TAR_PATH="${TEMP_OUTPUT_DIR}.tar.gz"
+	OUTPUT_DIR="${VERSIONS_DIR}/${REQUESTED_VERSION}"
 
 	mkdir -p $TEMP_DIR
-	rm -rf $TEMP_DIR/*
+	rm -rf "$TEMP_DIR/*"
 
-	echo "Downloading neovim from $DOWNLOAD_URL..."
-	curl -o $TAR_PATH -L "$DOWNLOAD_URL" -s
+	echo "Downloading neovim from ${DOWNLOAD_URL}..."
+	curl -o $TAR_PATH -L $DOWNLOAD_URL -s
 	xattr -c $TAR_PATH
 	echo "extracting files..."
 	tar xzf $TAR_PATH -C $TEMP_DIR
 
-	echo "installing neovim at $OUTPUT_DIR..."
+	echo "installing neovim at ${OUTPUT_DIR}..."
 	rm -rf $OUTPUT_DIR
 	mv $TEMP_OUTPUT_DIR $OUTPUT_DIR
 	rm -rf $TEMP_DIR
 
-	use $USER_VERSION
+	use $REQUESTED_VERSION
 }
 
 help() {

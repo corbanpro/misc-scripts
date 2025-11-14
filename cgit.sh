1b4c2805fd02bade34071d7a3102baf0bdf7da3a
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-14 12:31:20 -0700

    add check for which branches aren't on main

--- cgit.sh
+++ cgit.sh
@@ -5,15 +5,23 @@ dirs=(~/dev ~/.scripts ~/repos ~/.config ~/.config/nvim)
 
 push_mode=1
 commit_mode=1
+main_mode=1
 
 while [[ $# -gt 0 ]]; do
 	case "$1" in
 	-c)
 		push_mode=0
+		main_mode=0
 		shift
 		;;
 	-p)
 		commit_mode=0
+		main_mode=0
+		shift
+		;;
+	-m)
+		commit_mode=0
+		push_mode=0
 		shift
 		;;
 	esac
@@ -42,15 +50,31 @@ check_unpushed_commits() {
 	fi
 }
 
+# Function to check if the branch is on master
+check_branch_not_master() {
+	local dir="$1"
+	local branch=$(git -C "$dir" rev-parse --abbrev-ref HEAD 2>/dev/null)
+
+	if [ -z "$branch" ]; then
+		echo "Failed to get branch for $dir" >&2
+		return 1
+	fi
+
+	# Check if branch is NOT master or main
+	if [ "$branch" != "master" ] && [ "$branch" != "main" ]; then
+		echo "$dir"
+	fi
+}
+
 # Function to traverse directories and check for git repositories
 check_directory() {
 	local dir=$1
 	if [ -d "$dir/.git" ]; then
-		if [ "$mode" == "unpushed" ]; then
-			check_unpushed_commits "$dir"
-		else
-			check_uncommitted_changes "$dir"
-		fi
+		case "$mode" in
+		unpushed) check_unpushed_commits "$dir" ;;
+		uncommitted) check_uncommitted_changes "$dir" ;;
+		master) check_branch_not_master "$dir" ;;
+		esac
 	else
 		check_subdirectories "$dir"
 	fi
@@ -70,25 +94,24 @@ check_subdirectories() {
 	done
 }
 
-# Run the script
-if [[ "$commit_mode" -eq 1 ]]; then
-	echo "Checking for untracked changes"
+echo "Checking for untracked changes"
 
-	for d in "${dirs[@]}"; do
-		check_directory "$d"
-	done
-fi
+for d in "${dirs[@]}"; do
+	check_directory "$d"
+done
 
 mode="unpushed"
+echo
+echo "Checking for unpushed commits"
 
-if [[ "$commit_mode" -eq 1 && "$push_mode" -eq 1 ]]; then
-	echo
-fi
+for d in "${dirs[@]}"; do
+	check_directory "$d"
+done
 
-if [[ "$push_mode" -eq 1 ]]; then
-	echo "Checking for unpushed commits"
+mode="master"
+echo
+echo "Checking for branches not on main"
 
-	for d in "${dirs[@]}"; do
-		check_directory "$d"
-	done
-fi
+for d in "${dirs[@]}"; do
+	check_directory "$d"
+done

8ae4db9f27c4dc4d2bcaafc870a9d931ff891be8
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-14 13:25:54 -0700

    allow flexibility checking module versions

--- modversion.sh
+++ modversion.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 FILES=$(fd go.mod ~/dev)
+MODULE=$(echo "${1:-github.com/signalscode/go-shared}" | xargs)
 
 for F in $FILES; do
 	CONTENTS=$(cat $F)
-	if [[ $CONTENTS == *"github.com/signalscode/go-shared "* ]]; then
-		VERSION=$(cat $F | grep "github.com/signalscode/go-shared " | grep -v "replace" | cut -d" " -f2)
+	if [[ $CONTENTS == *"$MODULE "* ]]; then
+		VERSION=$(cat $F | grep "$MODULE " | grep -v "replace" | cut -d" " -f2)
 		echo "$(printf "%-60s" $(dirname $F)) $VERSION"
 	fi
 done

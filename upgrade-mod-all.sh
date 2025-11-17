2b671ecea500548d3d4edbe8817ee0895b3d65d5
Corban Procuniar <corban.procuniar@getsignals.ai>
2025-11-16 21:26:25 -0700

    restore old upgrade-mod-all

--- upgrade-mod-all.sh
+++ upgrade-mod-all.sh
@@ -11,9 +11,51 @@ for F in $FILES; do
 	echo -e "${C_CYAN}updating $F${C_RESET}"
 	mod
 
-	git commit -am "$COMMIT_MSG" >/dev/null
-	git push >/dev/null
+	if [ -n "$(git status --porcelain)" ]; then
+		git commit -am "$COMMIT_MSG" >/dev/null
 
-	gh pr create --title "$PR_NAME" --body "$COMMIT_MSG" --base main --head "$BRANCH_NAME" --draft >/dev/null ||
-		gh pr create --title "$PR_NAME" --body "$COMMIT_MSG" --base master --head "$BRANCH_NAME" --draft >/dev/null
+		git push >/dev/null
+
+		gh pr create --title "$PR_NAME" --body "$COMMIT_MSG" --base main --head "$BRANCH_NAME" --draft >/dev/null ||
+			gh pr create --title "$PR_NAME" --body "$COMMIT_MSG" --base master --head "$BRANCH_NAME" --draft >/dev/null
+	fi
 done
+
+# read -p "run update in $MODE mode? [Y/n]: " SHOULD_RUN
+#
+# if [[ $SHOULD_RUN == "n" ]]; then
+# 	exit 0
+# fi
+#
+# for F in $FILES; do
+# 	F=$(dirname $F)
+# 	cd $F
+# 	echo -e "${C_CYAN}updating $F${C_RESET}"
+# 	mod
+#
+# 	if [[ $MODE == "commit" ]]; then
+# 		if [ -n "$(git status --porcelain)" ]; then
+# 			CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# 			if [[ "$CURRENT_BRANCH" == "main" || "$CURRENT_BRANCH" == "master" ]]; then
+# 				echo "switching to branch: $BRANCH"
+# 				git checkout -b "$BRANCH_NAME" >/dev/null
+# 			fi
+# 			echo "committing changes"
+# 			git commit -am "$COMMIT_MSG" >/dev/null
+# 			git push >/dev/null
+# 			echo -e "${C_CYAN}Pushed changes for $F${C_RESET}"
+# 		else
+# 			echo -e "${C_CYAN}No update required for $F${C_RESET}"
+# 		fi
+# 	elif [[ $MODE == "push" ]]; then
+# 		if [ -n "$(git status --porcelain)" ]; then
+# 			REPO=$(gh repo view --json nameWithOwner -q .nameWithOwner)
+# 			gh api -X DELETE "repos/$REPO/git/refs/heads/$BRANCH" 2>&1 >/dev/null
+# 			git branch -D "$BRANCH" 2>&1 >/dev/null
+# 			gitchange "$BRANCH" "$PR_NAME" "$COMMIT_MSG"
+# 		else
+# 			echo -e "${C_CYAN}No update required for $F${C_RESET}"
+# 		fi
+# 	fi
+# done
+#

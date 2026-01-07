f125030e6e82a8d054f031b0de2e3ae4e168a33f
Corban Procuniar <corban.procuniar@getsignals.ai>
2026-01-07 09:09:12 -0700

    add podwatch script

---
+++ podwatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KUBE_COMMAND="kubectl get pods"
+
+GREP_PATTERNS=" -e NAME "
+
+if [ "$#" -gt 0 ]; then
+	for ARG in "$@"; do
+		GREP_PATTERNS+=" -e \"$ARG\" "
+	done
+
+	watch "$KUBE_COMMAND | grep $GREP_PATTERNS"
+else
+	watch "$KUBE_COMMAND"
+fi

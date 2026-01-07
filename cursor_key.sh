00a6111a479c29dc8490fe6f5305dba24b33292e
Corban Procuniar <corban.procuniar@getsignals.ai>
2026-01-06 17:34:14 -0700

    add cursor key functionality and cursor key karabiner config

--- cursor_key.sh
+++ cursor_key.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-say $1
+function single_tap {
+	osascript -e 'tell application "System Events" to keystroke "q" using {control down, command down}'
+}
+
+function double_tap {
+	say "double_tap"
+}
+
+function hold {
+	say "hold"
+}
+
+if [ "$1" == "single_tap" ]; then
+	single_tap
+elif [ "$1" == "double_tap" ]; then
+	double_tap
+elif [ "$1" == "hold" ]; then
+	hold
+fi
